89af7e577d853767817daef43d160ef203f85531
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-08 13:59:24 -0300

    added login page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+let loginName;
 const menu = document.querySelector('aside');
+const main = document.querySelector('main');
 
 function showMenu(){
     menu.classList.toggle('active');
-}
+    main.classList.toggle('grayscale');
+};
 
+function enter(){
+    const nameInput = document.querySelector('.login-button .type-here');
+
+    if(nameInput.value != '' && nameInput.value != undefined){
+        loginName = nameInput.value;
+        document.querySelector('.login-input').classList.add('hide');
+        setTimeout(() => {
+            document.querySelector('.login-screen').style.display = 'none'
+        }, 1000); 
+    }
+};
