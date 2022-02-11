7fe8a69bf5533648e2d1bad97644dffddfa4c09f
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-11 02:42:08 -0300

    added check on sidebar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 let loginName;
 let login;
-const menu = document.querySelector('aside');
-const main = document.querySelector('main');
+let messageTo='Todos';
+let privacy='Público';
+let privacyText=document.querySelector('.message-privacy');
+
+function selectParticipants(selection){
+    let selectionArray = selection.parentNode.querySelectorAll('.check-icon')
+    selectionArray.forEach(element => { 
+        element.classList.add('hide');
+    });
+    selection.querySelector('.check-icon').classList.toggle('hide');
+    
+    updateTo(selection);
+}
+function selectPrivacy(selection){
+    let selectionArray = selection.parentNode.querySelectorAll('.check-icon')
+    selectionArray.forEach(element => { 
+        element.classList.add('hide');
+    });
+    selection.querySelector('.check-icon').classList.toggle('hide');
+    
+    updatePrivacity(selection);
+}
+
+function updateTo(selection){
+    messageTo=selection.querySelector('p').textContent;
+    privacyText.innerHTML=`Enviando para ${messageTo} (${privacy})`;
+}
+function updatePrivacity(selection){
+    privacy=selection.querySelector('p').textContent;
+    privacyText.innerHTML=`Enviando para ${messageTo} (${privacy})`;
+}
 
 function showMenu(){
+    const menu = document.querySelector('aside');
+    const grayscale = document.querySelector('.grayscale');
     menu.classList.toggle('active');
-    main.classList.toggle('grayscale');
+    grayscale.classList.toggle('hide');
 };
 
 function enter(){
@@ -31,7 +62,7 @@ function enterChat(){
         document.querySelector('.login-screen').style.display = 'none'
     }, 1000); 
 
-    setInterval(updateParticipants,5000);
+    setInterval(updateParticipants,10000);
     setInterval(updateMessages,5000);
 }
 
@@ -56,7 +87,7 @@ function updateParticipants(){
 }
 /*get messages from serverMessage */
 function renderMessages(response){
-serverMessage=response.data;
+    serverMessage=response.data;
 
     for(let i=0;i<serverMessage.length;i++){
         let message = document.createElement('div');
@@ -67,18 +98,30 @@ serverMessage=response.data;
             `;
 
         document.querySelector('.messages').appendChild(message);
-        setTimeout(() => document.querySelector(".messages").scrollIntoView(false), 0);
+        document.querySelector(".messages").scrollIntoView(false);
     }
 };
 
 function renderparticipants(response){
     serverParticipants=response.data;
     let participant=document.querySelector('.contacts-list');
+
+    participant.innerHTML=`
+        <span data-identifier="participant" onclick="selectParticipants(this)">
+            <ion-icon name="people"></ion-icon>
+            <p>Todos</p>
+            <ion-icon class="check-icon" name="checkmark"></ion-icon>
+        </span>`;
+
     for(let i=0;i<serverParticipants.length;i++){
-        participant.innerHTML =`
-        <span><ion-icon name="people"></ion-icon>
-        <p>${serverParticipants[i].name}</p>
-        </span>
-        `
+        if(serverParticipants[i].name !== login.name){
+            participant.innerHTML +=`
+                <span data-identifier="participant" onclick="selectParticipants(this)">
+                    <ion-icon name="people"></ion-icon>
+                    <p>${serverParticipants[i].name}</p>
+                    <ion-icon class="check-icon hide" name="checkmark"></ion-icon>
+                </span>
+            `;
+        }
     }
 }
